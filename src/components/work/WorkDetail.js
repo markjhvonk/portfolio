74927a5f26808663d9b3f061cc7b5cddfbf0de42
Mark Vonk <markjhvonk@gmail.com>
2019-11-03 16:00:10 +0100

    Added slider to work detail

--- src/components/work/WorkDetail.js
+++ src/components/work/WorkDetail.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import XIcon from '../../assets/icons/icon-x.svg';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 class WorkDetail extends Component {
     render() {
         const { title, text, images, toggleModal } = this.props;
 
         return (
-            <div className="work-detail__container" onClick={toggleModal}>
+            <div className="work-detail__container">
                 <div className="work-detail">
                     <div className="work-detail__images">
-                        <div className="work-detail__slider">
+                        <Carousel
+                            showArrows={false}
+                            showThumbs={false}
+                            showIndicators={true}
+                            showStatus={false}
+                            autoPlay={true}
+                            interval={2000}
+                            infiniteLoop={true}
+                            dynamicHeight={false}>
                             <div className="work-detail__slide">
                                 <img src={images} alt="slider" />
                             </div>
-                        </div>
+                            <div className="work-detail__slide">
+                                <img src={images} alt="slider" />
+                            </div>
+                        </Carousel>
                     </div>
                     <div className="work-detail__content">
                         <div className="work-detail__close-btn" onClick={toggleModal}>
-                            <img src={XIcon} alt="close icon"/>
+                            <img src={XIcon} alt="close icon" />
                         </div>
                         <h2>
                             {title}
